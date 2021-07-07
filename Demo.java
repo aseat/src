e19f7e48646c2b6974fce59da31beab733d9b2ff
aseat <lemnlsdrye231mk@gmail.com>
2021-07-07 16:12:22 +0900

    演算子

--- Demo.java
+++ Demo.java
@@ -1,11 +1,10 @@
 import java.security.MessageDigest;
 class Demo {
         public static void main(String[] args) {
-                int X = 19;
-                if (X > 10) {
-                  System.out.println("X is greater than 10.");
-                } else {
-                  System.out.println("X is not greater than 10.");
-                }
+                System.out.println(10 + 5);
+                System.out.println(10 - 5);
+                System.out.println(2 * 3);
+                System.out.println(10 / 2);
+                System.out.println(10 % 3);
               }
 }
