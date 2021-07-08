d21fddb6fcf760006758c76bba8079796b2d208d
aseat <lemnlsdrye231mk@gmail.com>
2021-07-08 14:52:52 +0900

    浮動小数点型

--- Demo.java
+++ Demo.java
@@ -1,10 +1,11 @@
 import java.security.MessageDigest;
 class Demo {
         public static void main(String[] args) {
-                System.out.println(10 + 5);
-                System.out.println(10 - 5);
-                System.out.println(2 * 3);
-                System.out.println(10 / 2);
-                System.out.println(10 % 3);
+                System.out.println("* Float");
+                System.out.println(" Min: " + Float.MIN_VALUE);
+                System.out.println(" Max: " + Float.MAX_VALUE);
+                System.out.println("* Double");
+                System.out.println(" Min: " + Double.MIN_VALUE);
+                System.out.println(" Max: " + Double.MAX_VALUE);
               }
 }
