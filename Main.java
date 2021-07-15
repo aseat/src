847c221fd5290c3c3531374386f52e9ddf94695f
aseat <lemnlsdrye231mk@gmail.com>
2021-07-15 21:04:13 +0900

    敵キャラインスタンス生成

--- Main.java
+++ Main.java
@@ -4,9 +4,17 @@ public class Main {
     h.name = "鬼頭明里";
     h.hp = 100;
     System.out.println("勇者" + h.name + "を生み出しました");
+  Mantago m1 = new mantago();
+  m1.hp=50;
+  m1.suffix='A';
+  Mantago m2 = new mantago();
+  m1.hp=48;
+  m1.suffix='B';
+    
+  
    h.sit(5);
-   h.slip();
-   h.sit(25);
+   m1.run();
+   m2.run();
    h.run();
   }
 } 
